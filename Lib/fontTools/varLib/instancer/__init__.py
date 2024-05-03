f9136c7ec08c4710af7acf0aaedbd2acadf0fa8e
Behdad Esfahbod <behdad@behdad.org>
2024-05-03 07:59:54 -0600

    [instancer/avar2] Progress

--- Lib/fontTools/varLib/instancer/__init__.py
+++ Lib/fontTools/varLib/instancer/__init__.py
@@ -428,10 +428,6 @@ class AxisLimits(_BaseAxisLimits):
             )
 
 
-        if usingAvar and "avar" in varfont:
-            avar = varfont["avar"]
-            normalizedLimits = avar.renormalizeAxisLimits(normalizedLimits, varfont)
-
         fvarAxes = fvar.getAxes()
         for tag, triple in normalizedLimits.items():
             minV, defaultV, maxV = fvarAxes[tag]
@@ -448,7 +444,13 @@ class AxisLimits(_BaseAxisLimits):
                 *triple, distanceNegative, distancePositive
             )
 
-        return NormalizedAxisLimits(normalizedLimits)
+        normalizedLimits = NormalizedAxisLimits(normalizedLimits)
+
+        if usingAvar and "avar" in varfont:
+            avar = varfont["avar"]
+            normalizedLimits = avar.renormalizeAxisLimits(normalizedLimits, varfont)
+
+        return normalizedLimits
 
 
 class NormalizedAxisLimits(_BaseAxisLimits):
